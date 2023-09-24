83757f209db66b46536f433622d81cf1bb671c4f
kornel-kwiatkowski <kornel.kwiatkowski111@gmail.com>
2023-09-24 17:37:20 +0200

    first commit

---
+++ App.tsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import {
+  Button,
+  Image,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+function App(): JSX.Element {
+  return (
+    <SafeAreaView style={styles.safeArea}>
+      <View style={styles.container}>
+        <Image
+          source={require('./assets/bare-image.png')}
+          style={styles.image}
+        />
+        <Text style={styles.title}>Workshops</Text>
+        <Text style={styles.subtitle}>Bare React Native</Text>
+      </View>
+      <View style={styles.button}>
+        <Button
+          title="Sample Button"
+          onPress={() => {
+            console.log('No i czego klikasz, jak to nic nie robi');
+          }}
+        />
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+  image: {
+    width: 100,
+    height: 100,
+    borderRadius: 25,
+    marginBottom: 20,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 10,
+    color: 'black',
+  },
+  subtitle: {
+    fontSize: 16,
+    color: 'gray',
+    marginBottom: 20,
+  },
+  button: {
+    marginTop: 'auto',
+  },
+});
+
+export default App;
